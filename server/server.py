c0c11243110ea43a3442b8fbd1ab1045aad0cb81
TheVizWiz <vibhusiyer@gmail.com>
2024-02-19 14:26:31 -0500

    changed server script variable names

--- server/server.py
+++ server/server.py
@@ -3,13 +3,13 @@ import sys
 import serial
 import time
 
-arduino = serial.Serial(port=sys.argv[1], baudrate=115200, timeout=.1)
+arduino = serial.Serial(port=sys.argv[1], baudrate=115200, timeout=.01)
 
 
 while True:
-    num = input("enter a number: ")
+    command = input("enter a command: ")
     print("writing to arduino...")
-    arduino.write(bytes(num, "ascii"))
+    arduino.write(bytes(command, "ascii"))
     while arduino.in_waiting:
         time.sleep(0.001)
     return_message = arduino.readall()
