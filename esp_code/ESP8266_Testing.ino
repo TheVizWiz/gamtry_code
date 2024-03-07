de519893e1274d30dc1c5a469ba3b393b101d32a
Nishad-Thakar <159587185+Nishad-Thakar@users.noreply.github.com>
2024-03-06 20:36:16 -0500

    Add files via upload
    - Added sliders, graph, and buttons to website 
    - Added WebSocket implementation on ESP

--- esp_code/ESP8266_Testing.ino
+++ esp_code/ESP8266_Testing.ino
@@ -4,10 +4,14 @@
 #include <ESP8266mDNS.h>
 #include <ESP8266WebServer.h>
 #include <FS.h>   // Include the SPIFFS library
+#include <WebSocketsServer.h>
 
 ESP8266WiFiMulti wifiMulti;     // Create an instance of the ESP8266WiFiMulti class, called 'wifiMulti'
 
 ESP8266WebServer server(80);    // Create a webserver object that listens for HTTP request on port 80
+
+WebSocketsServer webSocket(81);    // create a websocket server on port 81
+
 
 File fsUploadFile;              // a File object to temporarily store the received file
 
@@ -18,28 +22,28 @@ void handleFileUpload();                // upload a new file to the SPIFFS
 void setup() {
   Serial.begin(115200);         // Start the Serial communication to send messages to the computer
   delay(10);
-  Serial.println('\n');
+  //Serial.println('\n');
 
   wifiMulti.addAP("HomeOffice", "home2453");   // add Wi-Fi networks you want to connect to
   wifiMulti.addAP("Gu_Net", "GaM.try!");
   wifiMulti.addAP("ssid_from_AP_3", "your_password_for_AP_3");
 
-  Serial.println("Connecting ...");
+  //Serial.println("Connecting ...");
   int i = 0;
   while (wifiMulti.run() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
     delay(1000);
-    Serial.print(++i); Serial.print(' ');
+    //Serial.print(++i); Serial.print(' ');
   }
-  Serial.println('\n');
-  Serial.print("Connected to ");
-  Serial.println(WiFi.SSID());              // Tell us what network we're connected to
-  Serial.print("IP address:\t");
-  Serial.println(WiFi.localIP());           // Send the IP address of the ESP8266 to the computer
+  //Serial.println('\n');
+  //Serial.print("Connected to ");
+  //Serial.println(WiFi.SSID());              // Tell us what network we're connected to
+  //Serial.print("IP address:\t");
+  //Serial.println(WiFi.localIP());           // Send the IP address of the ESP8266 to the computer
 
   if (MDNS.begin("gamtry")) {              // Start the mDNS responder for esp8266.local
-    Serial.println("mDNS responder started");
+    //Serial.println("mDNS responder started");
   } else {
-    Serial.println("Error setting up MDNS responder!");
+    //Serial.println("Error setting up MDNS responder!");
   }
 
   SPIFFS.begin();                           // Start the SPI Flash Files System
@@ -52,18 +56,19 @@ void setup() {
   server.on("/upload", HTTP_POST,                       // if the client posts to the upload page
     [](){},
     handleFileUpload                                    // Receive and save the file
-  );
+);
 
   server.onNotFound([]() {                              // If the client requests any URI
     if (!handleFileRead(server.uri()))                  // send it if it exists
       server.send(404, "text/plain", "404: Not Found"); // otherwise, respond with a 404 (Not Found) error
   });
-
+  startWebSocket();            // Start a WebSocket server 
   server.begin();                           // Actually start the server
-  Serial.println("HTTP server started");
+  //Serial.println("HTTP server started");
 }
 
 void loop() {
+  webSocket.loop(); 
   MDNS.update(); 
   server.handleClient();
 }
@@ -78,7 +83,7 @@ String getContentType(String filename) { // convert the file extension to the MI
 }
 
 bool handleFileRead(String path) { // send the right file to the client (if it exists)
-  Serial.println("handleFileRead: " + path);
+  //Serial.println("handleFileRead: " + path);
   if (path.endsWith("/")) path += "index.html";          // If a folder is requested, send the index file
   String contentType = getContentType(path);             // Get the MIME type
   String pathWithGz = path + ".gz";
@@ -88,10 +93,10 @@ bool handleFileRead(String path) { // send the right file to the client (if it e
     File file = SPIFFS.open(path, "r");                    // Open the file
     size_t sent = server.streamFile(file, contentType);    // Send it to the client
     file.close();                                          // Close the file again
-    Serial.println(String("\tSent file: ") + path);
+    //Serial.println(String("\tSent file: ") + path);
     return true;
   }
-  Serial.println(String("\tFile Not Found: ") + path);   // If the file doesn't exist, return false
+  //Serial.println(String("\tFile Not Found: ") + path);   // If the file doesn't exist, return false
   return false;
 }
 
@@ -100,7 +105,7 @@ void handleFileUpload(){ // upload a new file to the SPIFFS
   if(upload.status == UPLOAD_FILE_START){
     String filename = upload.filename;
     if(!filename.startsWith("/")) filename = "/"+filename;
-    Serial.print("handleFileUpload Name: "); Serial.println(filename);
+    //Serial.print("handleFileUpload Name: "); Serial.println(filename);
     fsUploadFile = SPIFFS.open(filename, "w");            // Open the file for writing in SPIFFS (create if it doesn't exist)
   } else if(upload.status == UPLOAD_FILE_WRITE){
     if(fsUploadFile)
@@ -108,12 +113,41 @@ void handleFileUpload(){ // upload a new file to the SPIFFS
   } else if(upload.status == UPLOAD_FILE_END){
     if(fsUploadFile) {                                    // If the file was successfully created
       fsUploadFile.close();                               // Close the file again
-      Serial.print("handleFileUpload Size: "); Serial.println(upload.totalSize);
+      //Serial.print("handleFileUpload Size: "); Serial.println(upload.totalSize);
       server.sendHeader("Location","/success.html");      // Redirect the client to the success page
       server.send(303);
     } else {
-      Serial.println("File upload failed");
+      //Serial.println("File upload failed");
       server.send(500, "text/plain", "500: couldn't create file");
     }
   }
+}
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) { // When a WebSocket message is received
+  switch (type) {
+    case WStype_DISCONNECTED:             // if the websocket is disconnected
+      //Serial.printf("[%u] Disconnected!\n", num);
+      break;
+    case WStype_CONNECTED: {              // if a new websocket connection is established
+        IPAddress ip = webSocket.remoteIP(num);
+        //Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+      }
+      break;
+    case WStype_TEXT:                     // if new text data is received
+      //Serial.printf("[%u] get Text: %s\n", num, payload);
+      //if (payload[0] == '#') {            // we get RGB data
+        uint32_t input = (uint32_t) strtol((const char *) &payload[1], NULL, 16); 
+        int x = input; 
+        Serial.printf("X%s", payload); 
+      //}
+      break;
+  }
+}
+
+
+
+
+void startWebSocket() { // Start a WebSocket server
+  webSocket.begin();                          // start the websocket server
+  webSocket.onEvent(webSocketEvent);          // if there's an incomming websocket message, go to function 'webSocketEvent'
+  //Serial.println("WebSocket server started.");
 }
